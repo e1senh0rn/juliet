38a605b49511b9f983f7d627cd2d049947047015
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2010-09-08 23:14:40 +0300

    Adding filtering.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -2,16 +2,26 @@ class ResponsesController < ApplicationController
   inherit_resources
   actions :index
   authorize_resource
+  respond_to :html, :xml
   
   def index
     @websites = Website.accessible_by(current_ability).all
     website_ids = @websites.map{ |s| s.id }
-    unless params[:website_id].blank?
-      raise CanCan::AccessDenied unless website_ids.include? params[:website_id].to_i
-      @responses = Response.includes(:website).where(:website_id => params[:website_id]).paginate(:page => params[:page])
-    else
-      @responses = Response.includes(:website).where(:website_id => website_ids).paginate(:page => params[:page])
+    @responses = Response.includes(:website).where(:website_id => website_ids)
+    
+    if params[:filters]
+      if params[:filters][:website_id]
+        raise CanCan::AccessDenied unless website_ids.include? params[:filters][:website_id].to_i
+        @responses = @responses.where(:website_id => params[:filters][:website_id])
+      end
+
+      if params[:filters][:is_good]
+        @responses = params[:filters][:is_good] == '1' ? @responses.good : @responses.bad
+      end
+      
     end
+    
+    @responses = @responses.paginate(:page => params[:page])
     super
   end
   
