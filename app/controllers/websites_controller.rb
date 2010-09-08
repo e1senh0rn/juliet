32985c6034b1c9ba1837441307d6872629e60a49
Dmitry Shaposhnik <dmitry@shaposhnik.name>
2010-09-08 23:14:32 +0300

    Make websites xml aware.

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,6 +1,7 @@
 class WebsitesController < ApplicationController
   inherit_resources
   authorize_resource
+  respond_to :html, :xml
   
   def index
     @websites = Website.accessible_by(current_ability).includes(:user).all
